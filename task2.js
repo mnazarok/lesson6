cf47793d6090519d6b4e50df12effe10880f8882
andy <mnazarok5@gmail.com>
2025-03-10 16:12:36 +0200

    Task 2

---
+++ task2.js
@@ -0,0 +1,14 @@
+let age;
+
+function check(age) {
+    if (age >= 18)
+        console.log("Є або дорівнює 18");
+    else 
+        console.log("Вам не має 18");
+}
+
+
+check(25);
+
+
+check(15);
