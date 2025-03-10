c067a14908b4d554cb915242d86984e4002821c1
andy <mnazarok5@gmail.com>
2025-03-10 15:57:47 +0200

    Task 1

---
+++ task1.js
@@ -0,0 +1,20 @@
+
+
+function area (a, b) {
+return console.log(a*b);
+}
+
+area(5,10);
+
+
+const area1 = function(a,b) {
+    return console.log(a*b);
+}
+
+area1 (5,11);
+
+
+const area2 = (a,b) => console.log(a*b);
+
+
+area2(5,12);
